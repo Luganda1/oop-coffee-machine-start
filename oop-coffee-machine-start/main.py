897fcb9356f69fa6d667611714b391d443f59f99
Luganda1 <62723878+Luganda1@users.noreply.github.com>
2021-05-21 12:22:16 -0700

    Initial commit

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,27 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+menu_item = MenuItem
+
+is_machine = True
+
+while is_machine:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_machine = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
